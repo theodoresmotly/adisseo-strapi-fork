4c49d08e2ba1d0add6b3ac5233c5b99d38c54c53
Théodore d'Avray <theodore@smotly.com>
2025-03-18 13:37:57 +0200

    fix: add remember me

--- packages/core/admin/admin/src/pages/Auth/components/Login.tsx
+++ packages/core/admin/admin/src/pages/Auth/components/Login.tsx
@@ -188,7 +188,7 @@ function LoginPage({ children }: LoginProps) {
       await defaultHandleLogin({
         email: values.email,
         password: values.password,
-        rememberMe: false,
+        rememberMe: values.rememberMe ?? false,
       });
     } catch (err) {
       setApiError(err instanceof Error ? err.message : 'Invalid token');
